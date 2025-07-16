50f34e71db39873b780404efa69423815774472d
Shahnaz Aktar Mazumder <168356002+shhnzzzz@users.noreply.github.com>
2025-07-16 11:06:49 +0530

    prepare.py

--- data/tagore/prepare.py
+++ data/tagore/prepare.py
@@ -4,9 +4,8 @@ import tiktoken
 import numpy as np
 
 # download the tiny shakespeare dataset
-input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
-if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+input_file_path = os.path.join(os.path.dirname(__file__), 'song.txt')
+
     with open(input_file_path, 'w', encoding='utf-8') as f:
         f.write(requests.get(data_url).text)
 
