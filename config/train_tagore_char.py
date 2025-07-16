1415d255905393f8f923a91e423387f48e841858
Shahnaz Aktar Mazumder <168356002+shhnzzzz@users.noreply.github.com>
2025-07-16 12:05:44 +0530

    train_tagore_char.py

--- config/train_tagore_char.py
+++ config/train_tagore_char.py
@@ -19,14 +19,14 @@ batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 6
-n_head = 6
+n_layer = 8 #6
+n_head = 8 # 6
 n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 50000
+lr_decay_iters = 50000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
